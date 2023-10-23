b5190169d64841f7fd61876e68c279c968e73dd5
Toyin <edgar-toyin1@hotmail.com>
2023-10-23 16:30:16 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was short, but also good.
+
+This time I have tried TailwindCss for the first time with only the documentation at hand, and now I understand why everyone says it comes pretty handy.
+
+I would like to know your thoughts!
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        "light-gray":"var(--light-gray)",
+        "grayish-blue":"var(--grayish-blue)",
+        "dark-blue":"var(--dark-blue)",
+      },
+      fontFamily: {
+        outfit: ['Outfit'],
+      }
+    },
+  },
+  plugins: [],
+}
+
